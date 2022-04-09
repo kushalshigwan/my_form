24a559e73031d51f2e73ee0390e7dae17465f105
unknown <kushalshigwan27@gmail.com>
2022-04-09 13:31:00 +0530

    Updates

---
+++ precache-manifest.e833e3e028397163bfde5a207cd494b8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf048f43eaff7f629e243b658f202601",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d9a3948e751c37f7a382",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "74b3bf46567877eb6e01",
+    "url": "/static/css/main.c1106d5f.chunk.css"
+  },
+  {
+    "revision": "d9a3948e751c37f7a382",
+    "url": "/static/js/2.3be1a04f.chunk.js"
+  },
+  {
+    "revision": "c1bdf306936ddd82567a345834b1f9d5",
+    "url": "/static/js/2.3be1a04f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74b3bf46567877eb6e01",
+    "url": "/static/js/main.87e7f177.chunk.js"
+  },
+  {
+    "revision": "bc4d8e71088c4b418a3a",
+    "url": "/static/js/runtime-main.c0e6038b.js"
+  }
+]);
